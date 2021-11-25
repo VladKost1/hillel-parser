2d672aab105169c228a19187b902ec3c920082fc
vladkost <vladkostiuk2@gmail.com>
2021-11-25 15:04:26 +0200

    added all tests

--- main.py
+++ main.py
@@ -14,10 +14,13 @@ def parse(query):
 if __name__ == '__main__':
 
     # Testing function `parse`
-    assert parse('https://youtube.com/?ivanguy=gorn&subscribe=true') == {'ivanguy': 'gorn', 'subscribe': 'true'}
-    assert parse('https://youtube.com/?ivanguy=gorn&subscribe=true&') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('https://youtube.com/?ivanguy=me&subscribe=true') == {'ivanguy': 'me', 'subscribe': 'true'}
+    assert parse('https://youtube.com/?ivanguy=me&subscribe=true&') == {'ivanguy': 'me', 'subscribe': 'true'}
     assert parse('https://youtube.com') == {}
     assert parse('https://youtube.com/?') == {}
-    assert parse('https://youtube.com/?ivanguy=gorn') == {'ivanguy': 'gorn'}
+    assert parse('https://youtube.com/?&&&') == {}
+    assert parse('https://youtube.com/?ivanguy=me') == {'ivanguy': 'me'}
     assert parse('https://youtube.com/?vlad=kost') == {'vlad': 'kost'}
     assert parse('https://youtube.com/?vlad=kost&age=20') == {'vlad': 'kost', 'age': '20'}
+    assert parse('https://youtube.com/?vlad=kost&age=20&&') == {'vlad': 'kost', 'age': '20'}
+    assert parse('https://youtube.com/?vlad=kost&age=20???') == {'vlad': 'kost', 'age': '20'}
