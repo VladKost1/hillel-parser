eab88c4f4b0b6f3577f3f00dcac4d5abb2337fdc
vladkost <vladkostiuk2@gmail.com>
2021-11-25 15:32:05 +0200

    added all test_cookies

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def parse_cookie(query):
             result[k] = v
     return result
 
+
 if __name__ == '__main__':
 
     assert parse_cookie('name=Vlad=User;age=20;') == {'name': 'Vlad=User', 'age': '20'}
@@ -14,3 +15,9 @@ if __name__ == '__main__':
     assert parse_cookie('name=Vlad;age=20;') == {'name': 'Vlad', 'age': '20'}
     assert parse_cookie('name=Vlad;') == {'name': 'Vlad'}
     assert parse_cookie('age=20') == {'age': '20'}
+    assert parse_cookie('name=Vlad=User;age=20=21;') == {'name': 'Vlad=User', 'age': '20=21'}
+    assert parse_cookie('name=Polina;age=unknown;') == {'name': 'Polina', 'age': 'unknown'}
+    assert parse_cookie('name=Vladislav=User=main;age=20;') == {'name': 'Vladislav=User=main', 'age': '20'}
+    assert parse_cookie('name=Vladislav=User;age=20;work=IT') == {'name': 'Vladislav=User', 'age': '20', 'work': 'IT'}
+    assert parse_cookie('name=Vladislav=User;age=20;work=IT=developer') == {'name': 'Vladislav=User', 'age': '20', 'work': 'IT=developer'}
+
